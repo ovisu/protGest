36566eb84fc89e6fc6606b36abb021e08af4d320
ovisu <71292373+ovisu@users.noreply.github.com>
2023-11-03 16:26:45 -0300

    Implementação funcionalides JS
    Implementação do menu mobile semi-funcional (pontos de ancoragem ainda não linkados as opções), implementação modal e slider de imagens responsivo (necessários ajustes nos menores tamanhos de tela.

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
 console.log("JavaScript is ready to run");
+
+// VARIAVEIS MODAL//
+const abreModal = document.querySelector(".btn-abre-modal");
+const fechaModal = document.querySelector(".btn-fecha-modal");
+const modal = document.querySelector("#modal");
+const modalLock = document.querySelector("body");
+
+//VARIAVEIS MENU//
+const menuMob = document.querySelector(".menu-mob");
+const menuDesk = document.querySelector(".menu-desk");
+
+//ABRE MODAL
+
+abreModal.addEventListener("click", () => {
+  modal.showModal();
+  modalLock.classList.add("scroll-lock");
+});
+
+fechaModal.addEventListener("click", () => {
+  modal.close();
+  modalLock.classList.remove("scroll-lock");
+});
+
+//CAROUSEL
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+//MENU MOBILE//
+
+menuMob.addEventListener("click",()=>{
+
+    menuDesk.classList.toggle("hidden-menu")
+
+})
